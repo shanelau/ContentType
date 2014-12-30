7fe6955550631dd2b9438b27e58141da3f761626
liuxing <shanelau1021@gmail.com>
2014-12-30 14:57:27 +0800

    init

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2014 Meizu bigertech, All rights reserved.
+ * http://www.bigertech.com/
+ * @author liuxing
+ * @date  14-12-30
+ * @description
+ *
+ */
+var contentType = require('../lib/index');
+console.log(contentType('a.jpg'));
